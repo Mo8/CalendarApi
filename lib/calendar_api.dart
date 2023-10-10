d8603631c3334e2226b025d595b70bbd0e7540f5
Mo8 <juliancautela@gmail.com>
2023-10-10 15:27:13 +0200

    Revert "Revert "better export""
    This reverts commit d2700f8177524f5bed0a95a04437a7a858602d89.

--- lib/calendar_api.dart
+++ lib/calendar_api.dart
@@ -1,9 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/calendar_api.dart';
 export 'src/model/calendar.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/model/event.dart';
