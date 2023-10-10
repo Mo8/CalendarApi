44ddfc801b43bc1c7bdf326b82229f0704f7101a
Mo8 <juliancautela@gmail.com>
2023-10-10 16:06:15 +0200

    bug with mars shift

--- lib/src/calendar_api.dart
+++ lib/src/calendar_api.dart
@@ -4,11 +4,12 @@ class CalendarApi{
   static Calendar getCalendar(int year,int month){
     if(month < 1 || month > 12) throw Exception("Month must be between 1 and 12");
 
-    DateTime dateStart = DateTime(year, month);
-    DateTime dateEnd = DateTime(year, month + 1);
-
-
-    return Calendar(List.generate(dateEnd.difference(dateStart).inDays, (index) => dateStart.add(Duration(days: index))));
+    List<DateTime> days = [DateTime(year,month,1)];
+    while(days.last.month == month){
+      days.add(DateTime(year,month,days.length + 1));
+    }
+    days.removeLast();
+    return Calendar(days);
   }
 
   static void addEvent(Calendar calendar, Event event){
