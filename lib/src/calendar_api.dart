935330698d31dc14195a736c692051074e02bff5
Mo8 <juliancautela@gmail.com>
2023-10-10 21:46:36 +0200

    add doc

--- lib/src/calendar_api.dart
+++ lib/src/calendar_api.dart
@@ -5,6 +5,10 @@ class CalendarApi{
   ///
   /// Get a calendar for a specific year and month
   ///
+  /// [month] must be between 1 and 12
+  ///
+  /// CalendarApi.getCalendar(2023, 1);
+  ///
   static Calendar getCalendar(int year,int month){
     if(month < 1 || month > 12) throw Exception("Month must be between 1 and 12");
 
@@ -18,6 +22,10 @@ class CalendarApi{
 
   ///
   /// Add an event to a calendar
+  ///
+  /// [event] must be between the first and the last day of the calendar
+  ///
+  /// CalendarApi.addEvent(calendar, Event("RDV", "Petite description", DateTime(2023, 1, 1, 10), DateTime(2023, 1, 1, 11)));
   ///
   static void addEvent(Calendar calendar, Event event){
     if(event.start.isBefore(calendar.days.first) || event.end.isAfter(calendar.days.last)) {
